eb8aa6369eefda835e37498f4833c9e6225f4c33
Alejo Lovallo <alejoe.lovallo@gmail.com>
2024-07-03 18:16:13 -0300

    migrations

---
+++ src/migrations/1720041282058-CreateAdmin.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateAdmin1720041282058 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
