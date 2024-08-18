ef2402c624275a9b3f59210938c3adfe0f944224
Alejo Lovallo <alejoe.lovallo@gmail.com>
2024-08-18 01:01:28 -0300

    fix migrations

--- src/migrations/002_create_admin_user.sql
+++ src/migrations/002_create_admin_user.sql
@@ -4,5 +4,9 @@ npx ts-node src/scripts/hashPassword -p <password>
 ```
 
 INSERT INTO public.users(
-	mail, name, surname, password, birth_date, active, roles, country)
-	VALUES ('admin@admin.com', 'admin', 'admin', 'admin', '1998-01-01 00:00:00', true, 'ADMIN', '472ab272-3963-439a-8881-ac869e6b8a72');
+	mail, name, surname, password, birth_date, active, role, country)
+	VALUES ('admin@admin.com', 'admin', 'admin', 'admin', '1998-01-01 00:00:00', true, 'ADMIN', 'b83ca496-f470-4563-93e9-9abf27963b8f');
+
+INSERT INTO public.users(
+	mail, name, surname, password, birth_date, active, role, country)
+	VALUES ('example@gmail.com', 'example', 'example', 'example', '1998-01-01 00:00:00', true, 'USER', 'b83ca496-f470-4563-93e9-9abf27963b8f');
